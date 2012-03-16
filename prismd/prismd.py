db891fa1f9cd267f43c4c3d2b646430ea6d43fa3
Fred Hatfull <fred.hatfull@gmail.com>
2012-03-15 18:05:59 -0700

    stupid indentation

--- prismd/prismd.py
+++ prismd/prismd.py
@@ -80,7 +80,7 @@ class RandomHandler(LightsHandler):
             self.write(str(i))
             for n in xrange(48):
                 self.set_light(n, {'r': random.randint(0,15), 'g': random.randint(0,15), 'b': random.randint(0,15), 'i':255})
-        time.sleep(1)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
